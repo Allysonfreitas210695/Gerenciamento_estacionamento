8b62128f416c7121d86ed97e294400d943dc51c8
Allysonfreitas210695 <alisonfr83@gmail.com>
2024-02-08 22:09:23 -0300

    ajuste na rotas de autenticação e refresh token

--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView  # Importa a view de refresh token
 from gerenciamento_estacionamento.views import  ClienteViewSet, VeiculoViewSet, EstacionamentoViewSet, TarifaViewSet
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
@@ -13,7 +14,8 @@ router.register(r'Tarifas', TarifaViewSet, basename='tarifas')
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/token-auth/", views.obtain_auth_token),
+    path("api/token/", TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Rota para obter o token de acesso
+    path("api/token-refresh/", TokenRefreshView.as_view(), name='token_refresh'),  # Rota para refresh token
     path("api/", include(router.urls)),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
