926c747674a47d315d7ced60c0b970477508096b
Allysonfreitas210695 <alisonfr83@gmail.com>
2024-02-09 21:09:30 -0300

    adicionando cors a aplicação

--- gerenciamento_estacionamento/views.py
+++ gerenciamento_estacionamento/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Cliente, Veiculo, Estacionamento, Tarifa
 from .serializers import ClienteSerializer, VeiculoSerializer, EstacionamentoSerializer, TarifaSerializer
 
